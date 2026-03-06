62b85dc9393bda420b57f6b9e5010d7b1627aa6a
willianIFPA <willian.virgilio@ifpa.edu.br>
2026-03-06 19:46:48 -0300

    teste

--- src/pkg3nd_pontosdimensionais/Main.java
+++ src/pkg3nd_pontosdimensionais/Main.java
@@ -6,6 +6,10 @@ public class Main {
 
 
     public static void main(String[] args) {
+        DoisPontos dois = new DoisPontos(1,3);
+        System.out.println(dois.toString());
+        
+        
         
     }
     
